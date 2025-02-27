5c70ba2824cc24c193fc48d4974aa3e5d591a328
AaronOmordha <aaronomordha@gmail.com>
2025-02-27 13:12:26 +0000

    Further Basline Code

---
+++ Application.h
@@ -0,0 +1,17 @@
+#ifndef Application_H
+#define Application_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Application{
+    private:
+
+    public:
+
+    protected:
+
+};
+
+#endif
---
+++ Game.h
@@ -0,0 +1,17 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Game{
+    private:
+
+    public:
+
+    protected:
+
+};
+
+#endif
